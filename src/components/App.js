490bc3d3fe907a6a43563b1ed25dfd47a3dd9bf3
andrewjumperdev <andrew.jumper.ent@gmail.com>
2023-06-05 16:50:29 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Banner from "./Banner";
+import Cart from "./Carts";
+
+function App() {
+  return (
+    <Banner>
+      <Cart />
+    </Banner>
+  );
+}
+
+export default App;
---
+++ src/components/Carts.js
@@ -0,0 +1,5 @@
+function Cart() {
+    return <p>Je suis le Cart</p>
+}
+
+export default Cart
